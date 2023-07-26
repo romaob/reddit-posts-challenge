0d6536f96044f40713eb54e676aebb9af0063b1f
Romao Barbosa <romaolb@outlook.com>
2023-07-26 14:17:39 -0300

    Initial Commit - Basic app - React Navigation implemented - Two basic screens implemented

---
+++ App.tsx
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+
+import Home from './src/screens/Home';
+import Post from './src/screens/Post';
+
+const Stack = createNativeStackNavigator();
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Post" component={Post} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
